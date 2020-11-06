6199e794c3653fed5fc2a727705d11d50c88d5a8
D3v <deviljin112@users.noreply.github.com>
2020-11-06 11:53:31 +0000

    Phase 2

--- calculator.py
+++ calculator.py
@@ -19,13 +19,34 @@ class Calculator:
     def multiply(self):
         return self.num_1 * self.num_2
 
+    @property
+    def divisible(self):
+        if self.num_1 % self.num_2 == 0:
+            return True
+        else:
+            return False
+
+    @property
+    def triangle(self):
+        area = (self.num_1 * self.num_2) / 2
+        return area
+
+    @property
+    def converter(self):
+        rate = 2.54
+
+        if self.num_1 != 0:
+            return self.num_1 * rate
+        elif self.num_2 != 0:
+            return self.num_2 / rate
+
 
 def main():
     print("-=CALCULATOR=-")
 
     try:
         first, second = input(
-            "What two numbers would you like to use?\nFormat: NUM NUM\n=>"
+            "What two numbers would you like to use?\nFormat: NUM NUM\nFor conversion use first NUM for cm to inch, or second for inch to cm\nUse 0 for the number you dont want to convert\n=>"
         ).split(" ")
     except:
         print("You need to provide two numbers in 'NUM NUM' format!!")
@@ -37,7 +58,7 @@ def main():
 
             while True:
                 choice = input(
-                    "What would you like to do?\nAdd / Subtract / Divide / Multiply\n=> "
+                    "What would you like to do?\nAdd / Subtract / Divide / Multiply / Divisible / Triangle / Converter\n=> "
                 )
 
                 if choice.lower() == "add":
@@ -51,6 +72,15 @@ def main():
                     break
                 elif choice.lower() == "multiply":
                     print(calc.multiply)
+                    break
+                elif choice.lower() == "divisible":
+                    print(calc.divisible)
+                    break
+                elif choice.lower() == "trianlge":
+                    print(calc.triangle)
+                    break
+                elif choice.lower() == "converter":
+                    print(calc.converter)
                     break
                 else:
                     print("This option is not available.\nTry again!")
