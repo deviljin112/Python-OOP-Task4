036ff9126bdd32c8b73b981e36c5f410a2549410
D3v <deviljin112@users.noreply.github.com>
2020-11-06 15:33:06 +0000

    Fully functional calculator

--- calculator.py
+++ calculator.py
@@ -1,92 +1,174 @@
+import os
+
+os.system("color")
+
+
 class Calculator:
-    def __init__(self, num_1, num_2):
-        self.num_1 = num_1
-        self.num_2 = num_2
+    def __init__(self, numbers):
+        self.numbers = numbers
 
     @property
     def add(self):
-        return self.num_1 + self.num_2
+        answer = 0
+        for i in self.numbers:
+            answer += i
+        return answer
 
     @property
     def subtract(self):
-        return self.num_1 - self.num_2
+        answer = self.numbers[0]
+        for i in self.numbers:
+            answer -= i
+        return answer
 
     @property
     def divide(self):
-        return self.num_1 / self.num_2
+        answer = self.numbers[0]
+        for i in self.numbers:
+            answer /= i
+        return answer
 
     @property
     def multiply(self):
-        return self.num_1 * self.num_2
+        answer = self.numbers[0]
+        for i in self.numbers:
+            answer *= i
+        return answer
+
+    @property
+    def modulus(self):
+        return self.numbers[0] % self.numbers[1]
+
+    @property
+    def power(self):
+        return self.numbers[0] ** self.numbers[1]
 
     @property
     def divisible(self):
-        if self.num_1 % self.num_2 == 0:
+        if self.numbers[0] % self.numbers[1] == 0:
             return True
         else:
             return False
 
     @property
     def triangle(self):
-        area = (self.num_1 * self.num_2) / 2
+        area = (self.numbers[0] * self.numbers[1]) / 2
         return area
 
-    @property
-    def converter(self):
+    def converter(self, choice):
         rate = 2.54
 
-        if self.num_1 != 0:
-            return self.num_1 * rate
-        elif self.num_2 != 0:
-            return self.num_2 / rate
+        if choice == "cm":
+            return self.numbers[0] * rate
+        elif choice == "inch":
+            return self.numbers[0] / rate
+
+
+options = ["add", "subtract", "divide", "multiply"]
+options_2 = ["modulus", "power", "triangle", "divisible"]
+options_3 = ["convert"]
+
+END = "\033[0m"
+RED = "\033[91m"
+CYAN = "\033[96m"
 
 
 def main():
-    print("-=CALCULATOR=-")
+    print(CYAN, "-=CALCULATOR=-", END)
 
-    try:
-        first, second = input(
-            "What two numbers would you like to use?\nFormat: NUM NUM\nFor conversion use first NUM for cm to inch, or second for inch to cm\nUse 0 for the number you dont want to convert\n=>"
-        ).split(" ")
-    except:
-        print("You need to provide two numbers in 'NUM NUM' format!!")
-    else:
-        if first.isdigit() and second.isdigit():
-            first, second = int(first), int(second)
+    print(
+        f"""
+What would you like to do?
 
-            calc = Calculator(first, second)
+Multi digit operations: {RED}{", ".join(options).title()}{END}
+Two digit operations: {RED}{", ".join(options_2).title()}{END}
+Single digit operations: {RED}{", ".join(options_3).title()}{END}
 
-            while True:
-                choice = input(
-                    "What would you like to do?\nAdd / Subtract / Divide / Multiply / Divisible / Triangle / Converter\n=> "
-                )
+NOTE: Subtraction and Division will use the first number
+        as the base for its operations.
+
+"""
+    )
+
+    choice = input("=> ")
+
+    if choice.lower() in options:
+        print(f"Please input your numbers.\nFormat: {CYAN}NUM NUM NUM ... NUM{END}")
+        numbers = input("=> ").split(" ")
+
+        if len(numbers) > 1:
+            can_continue = True
+            for i in range(len(numbers)):
+                if numbers[i].isdigit():
+                    numbers[i] = int(numbers[i])
+                else:
+                    can_continue = False
+                    break
+
+            if can_continue:
+                calc = Calculator(numbers)
 
                 if choice.lower() == "add":
-                    print(calc.add)
-                    break
+                    print(f"Answer is: {CYAN}{calc.add}{END}")
                 elif choice.lower() == "subtract":
-                    print(calc.subtract)
-                    break
-                elif choice.lower() == "divide":
-                    print(calc.divide)
-                    break
+                    print(f"Answer is: {CYAN}{calc.subtract}{END}")
                 elif choice.lower() == "multiply":
-                    print(calc.multiply)
-                    break
-                elif choice.lower() == "divisible":
-                    print(calc.divisible)
-                    break
-                elif choice.lower() == "trianlge":
-                    print(calc.triangle)
-                    break
-                elif choice.lower() == "converter":
-                    print(calc.converter)
-                    break
-                else:
-                    print("This option is not available.\nTry again!")
+                    print(f"Answer is: {CYAN}{calc.multiply}{END}")
+                elif choice.lower() == "divide":
+                    print(f"Answer is: {CYAN}{calc.divide}{END}")
 
+            else:
+                print(f"You need to provide an {RED}INTEGER{END}")
         else:
-            print("You need to provide two integer numbers!")
+            print(f"You need to input more than {RED}1{END} number")
+
+    elif choice.lower() in options_2:
+        print(f"Please input 2 numbers.\nFormat: {CYAN}NUM NUM{END}")
+        numbers = input("=> ").split(" ")
+
+        if len(numbers) == 2:
+            if numbers[0].isdigit() and numbers[1].isdigit():
+                first, second = int(numbers[0]), int(numbers[1])
+
+                calc = Calculator([first, second])
+
+                if choice.lower() == "divisible":
+                    print(f"Answer is: {CYAN}{calc.divisible}{END}")
+                elif choice.lower() == "triangle":
+                    print(f"Answer is: {CYAN}{calc.triangle}{END}")
+                elif choice.lower() == "power":
+                    print(f"Answer is: {CYAN}{calc.power}{END}")
+                elif choice.lower() == "modulus":
+                    print(f"Answer is: {CYAN}{calc.modulus}{END}")
+
+            else:
+                print(f"Please provide an {RED}INTEGER{END}")
+        else:
+            print(f"Please provide {RED}TWO{END} numbers.")
+
+    elif choice.lower() in options_3:
+        print("Would you like to convert cm to inch or inch to cm?")
+        choice2 = input(
+            f"Options:\n {CYAN}'cm'{END} to inch\n {CYAN}'inch'{END} to cm.\n=> "
+        )
+        if choice2.lower() == "cm" or choice2.lower() == "inch":
+            number = input("Please provide the number to convert.\n=> ")
+
+            if number.isdigit():
+                number = int(number)
+                calc = Calculator([number])
+
+                if choice2.lower() == "cm":
+                    print(f"Answer is: {CYAN}{calc.converter('cm')}{END}")
+                elif choice2.lower() == "inch":
+                    print(f"Answer is: {CYAN}{calc.converter('inch')}{END}")
+            else:
+                print(f"Please provide a single {RED}INTEGER{END}")
+        else:
+            print(f"That's not an options.\n{RED}Please try again!{END}")
+
+    else:
+        print(f"That's not an options.\n{RED}Please try again!{END}")
 
 
 if __name__ == "__main__":
